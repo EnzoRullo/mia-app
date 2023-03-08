85cb0fc04b15dd919039b960e44aef931ba64c71
EnzoRullo <lorenzo.rullo27@gmail.com>
2023-03-08 10:33:04 +0100

    finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnimaliComponent } from './animali/animali.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimaliComponent
   ],
   imports: [
     BrowserModule
